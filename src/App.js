983a2ef31446711b2ff40a5888e172a74fa17478
leonsua@gmail.com <leonsua@gmail.com>
2020-02-26 18:39:55 -0500

    Lists & State

--- src/App.js
+++ src/App.js
@@ -14,19 +14,10 @@ class App extends Component {
     showPersons: false
   };
 
-
-  switchNameHandler = (newName) => {
-    // console.log('What is clicked!!')
-    // DON'T DO THIS: this.state.persons[0].name = "Beto";
-    this.setState(
-      {
-        persons: [
-          { name: newName, age: 41 },
-          { name: "Leon", age: 41 },
-          { name: "Nena", age: 19 }
-        ]
-      }
-    )
+  deletePersonHandler = (personsIndex) => {
+    const persons = this.state.persons;
+    persons.splice(personsIndex, 1);
+    this.setState({ persons: persons });
   }
 
   nameChangedHandler = (event) => {
@@ -61,8 +52,9 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map(person => {
+          {this.state.persons.map((person, index) => {
             return <Person
+              click = { () =>  this.deletePersonHandler(index) }
               name={person.name}
               age={person.age}
             />
