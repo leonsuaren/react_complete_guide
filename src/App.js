c05670e67cdea0e7168b314d26d33839ffa6d7de
leonsua@gmail.com <leonsua@gmail.com>
2020-02-25 19:28:00 -0500

    Handling Dynamic Content 'The JavaScript Way'

--- src/App.js
+++ src/App.js
@@ -56,6 +56,27 @@ class App extends Component {
       cursor: 'pointer'
     }
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age} />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Beto!')}
+            change={this.nameChangedHandler}
+          >My Hobbies: Parachute</Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age} />
+        </div>
+      )
+    }
+
     return (
       <div className="App">
         <h1>Hi!! I'm a React App</h1>
@@ -63,23 +84,7 @@ class App extends Component {
         <button
           style={style}
           onClick={this.togglePersonHandler} >Toggle Persons</button>
-        {
-          this.state.showPersons === true ?
-          <div>
-            <Person
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age} />
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-              click={this.switchNameHandler.bind(this, 'Beto!')}
-              change={this.nameChangedHandler}
-            >My Hobbies: Parachute</Person>
-            <Person
-              name={this.state.persons[2].name}
-              age={this.state.persons[2].age} />
-          </div> : null
-        }
+          { persons }
       </div>
     );
     // return React.createElement('div', { className: 'App'}, React.createElement('h1', null, 'Does this Work?'))
