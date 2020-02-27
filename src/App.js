e939eee5b9c9c2276eac58076916e1b439ec93e9
leonsua@gmail.com <leonsua@gmail.com>
2020-02-26 19:27:01 -0500

    Flexible List

--- src/App.js
+++ src/App.js
@@ -21,16 +21,22 @@ class App extends Component {
     this.setState({ persons: persons });
   }
 
-  nameChangedHandler = (event) => {
-    this.setState(
-      {
-        persons: [
-          { name: 'Roberto', age: 41 },
-          { name: event.target.value, age: 41 },
-          { name: "Nena", age: 19 }
-        ]
-      }
-    )
+  nameChangedHandler = ( event , id ) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+    // const person = Object.assign({}, this.state.persons[personIndex]) es lo mismo que las lineas de codigo de arriba
+
+    person.name = event.target.value
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons })
   }
 
   togglePersonHandler = () => {
@@ -59,6 +65,7 @@ class App extends Component {
               name={person.name}
               age={person.age}
               key = {person.id}
+              change = { (event) =>  this.nameChangedHandler(event, person.id) }
             />
           })}
         </div>
